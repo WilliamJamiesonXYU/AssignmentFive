d871ceb06ef84c45cb7de4b63cdc13b3c5eaa862
William Jamieson <jamiewilliamson@protonmail.com>
2024-04-05 15:25:17 -0400

    Please work

--- src/QuadrantTree.java
+++ src/QuadrantTree.java
@@ -3,10 +3,58 @@ public class QuadrantTree {
     private QTreeNode root;
 
     public QuadrantTree(int[][] thePixels) {
-        buildTree(root, thePixels.length, thePixels, 0);
+        if (thePixels.length == 1) {
+            root = new QTreeNode(null, 0, 0, thePixels.length, Gui.averageColor(thePixels, 0, 0, thePixels.length));
+        }
+        else {
+            root = new QTreeNode();
+            root.setColor(Gui.averageColor(thePixels, 0, 0, thePixels.length));
+            root.setSize(thePixels.length);
+            buildTree(root, thePixels.length, thePixels, 0);
+        }
     }
 
-    private void buildTree (QTreeNode parent, int quadrantSize, int[][] quadrantPixels, int quadrantPos) {
+    private void buildTree (QTreeNode parent, int[][] thePixels) {
+        //Base case: No children to be made
+        if (parent.getSize() == 1) {
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            int subQuadrantSize = parent.getSize() / 2;
+            int childX;
+            int childY;
+            if (i == 0) {
+                childX = parent.getx();
+                childY = parent.gety();
+            }
+            else if (i == 1) {
+                childX = parent.getx() + subQuadrantSize;
+                childY = parent.gety();
+            }
+            else if (i == 2) {
+                childX = parent.getx();
+                childY = parent.gety() + subQuadrantSize;
+            }
+            else {
+                childX = parent.getx() + subQuadrantSize;
+                childY = parent.gety() + subQuadrantSize;
+            }
+            int childColor = Gui.averageColor(thePixels, childX, childY, subQuadrantSize);
+            QTreeNode child;
+            if (subQuadrantSize == 1) {
+                child = new QTreeNode(null, childX, childY, subQuadrantSize, childColor);
+            }
+            else {
+                child = new QTreeNode();
+                child.setx(childX);
+                child.sety(childY);
+                child.setSize(subQuadrantSize);
+                child.setColor(childColor);
+                buildTree(child, thePixels);
+            }
+            parent.setChild(child, i);
+
+        }
 //
 //        // Base case: root node
 //        if (this.root == null) {
